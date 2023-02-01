f01d277226e68e10827c52970597805b1d716b9a
Elijah Gayot <egayot39@academic.rrc.ca>
2023-02-01 13:57:48 -0600

    First Commit

---
+++ src/pages/LogIn.js
@@ -0,0 +1,17 @@
+function LogIn() {
+  return (
+    <div>
+      <div>
+        <h2>Log in to your Account</h2>
+      </div>
+      <div>
+        <input placeholder="Email"></input>
+      </div>
+      <div>
+        <input placeholder="Password"></input>
+      </div>
+    </div>
+  );
+}
+
+export default LogIn;
